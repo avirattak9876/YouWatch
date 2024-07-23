cc0e4ea4f3c7ac35c232d4892f5e62da4c168ba1
Avirat Tak <avirattak9876@gmail.com>
2024-07-23 19:18:06 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import 'dotenv/config';  // Load environment variables
 import connectDB from './db/connect.js';
+import app from './app.js';
 
 console.log('Starting application...');
 
 // Connect to the database
 connectDB().then(() => {
-    console.log('Connection established, proceeding with application logic...');
+    app.listen(process.env.PORT || 8000,()=>{
+         console.log(`server has been started at ${process.env.PORT}`);
+    })
 }).catch((err) => {
     console.error('Failed to establish connection:', err);
 });
